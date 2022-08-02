004fa38d1a1f4682d39faaa3af6502aa15ca0790
Madhuri Singh <54738079+Mad-Singh@users.noreply.github.com>
2022-08-03 04:18:37 +0530

    Create diamondPattern.java

---
+++ diamondPattern.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc= new Scanner(System.in);
+	    int n= sc.nextInt();
+	   
+	    for(int i=1;i<=n;i++){
+	        
+	        for(int j=1;j<=n-i;j++){
+	            System.out.print(" ");
+	        }
+	        int stars=(2*i)-1;
+	        for(int j=1; j<=stars; j++){
+	            System.out.print("*");
+	        }
+	        System.out.println(" ");
+	    }
+	     for(int i=n;i>0;i--){
+	        
+	        for(int j=1;j<=n-i;j++){
+	            System.out.print(" ");
+	        }
+	        int stars=(2*i)-1;
+	        for(int j=1; j<=stars; j++){
+	            System.out.print("*");
+	        }
+	        System.out.println(" ");
+	    }
+	}
+}
