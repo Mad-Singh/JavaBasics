30720de054690498cc9103e30219cb25cfe07411
Madhuri Singh <54738079+Mad-Singh@users.noreply.github.com>
2022-08-08 04:34:17 +0530

    Create UsingIndexOf.java

---
+++ UsingIndexOf.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class UsingIndexOf{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    String emailAddress = sc.next();
+	    String userName= emailAddress.substring(0,emailAddress.indexOf("@"));
+	    System.out.println(userName);
+	}
+}
