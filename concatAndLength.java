29e116ef8a62366aef192d203833297e99f6bc5c
Madhuri Singh <54738079+Mad-Singh@users.noreply.github.com>
2022-08-07 06:10:03 +0530

    Create concatAndLength.java

---
+++ concatAndLength.java
@@ -0,0 +1,12 @@
+import java.util.*;
+ 
+public class Main {
+   public static void main(String args[]) {
+       Scanner sc= new Scanner(System.in);
+       String firstInput = sc.nextLine();
+       String secondInput = sc.nextLine();
+       String outputStr= firstInput+secondInput;
+       System.out.println(outputStr.length());
+   }
+}
+
