c81fbacd8e83d46de2438f8d65bc042cc9ecb022
Madhuri Singh <54738079+Mad-Singh@users.noreply.github.com>
2022-08-07 20:52:23 +0530

    Create ChangeEToI.java

---
+++ ChangeEToI.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class ChangeEToI
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    String inputStr = sc.next();
+	    String[] arr = inputStr.split("");
+	    for(int i=0;i<inputStr.length();i++){
+	        if(arr[i].compareTo("e") == 0){
+	            arr[i] = "i";
+	        }
+	    }
+	    String outputStr="";
+	    
+	    for(int i=0; i<inputStr.length();i++){
+	        outputStr+= arr[i];
+	    }
+	    
+	    
+	    System.out.println(outputStr);
+	    
+	}
+}
