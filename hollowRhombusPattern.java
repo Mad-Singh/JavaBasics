eecdbd74d2f3778686d24bc2f2f3464cd81743d5
Madhuri Singh <54738079+Mad-Singh@users.noreply.github.com>
2022-09-08 21:55:05 +0530

    Update hollowRhombusPattern.java

--- hollowRhombusPattern.java
+++ hollowRhombusPattern.java
@@ -7,7 +7,6 @@ public class Main
 	    int n= sc.nextInt();
 	   
 	    for(int i=1;i<=n;i++){
-	        
 	        for(int j=1;j<=n*2-1;j++){
 	            if(i+j==n+1 || i+j==n*2 || (i==1 && j>=n) || (i==n && j<=n)) {
 	                System.out.print("*");
