737a2e468aa7b5c2bfe6622317fb5e52d97a1c7b
Madhuri Singh <54738079+Mad-Singh@users.noreply.github.com>
2022-09-10 23:03:59 +0530

    Update ReplaceE.java

--- ReplaceE.java
+++ ReplaceE.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class ReplaceE{
 	public static void main(String[] args) {
+		
 	    Scanner sc = new Scanner(System.in);
 	    StringBuilder input = new StringBuilder(sc.nextLine());
 	    for(int i=0;i<input.length();i++){
